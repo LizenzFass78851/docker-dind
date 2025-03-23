dafba4092e2235e681262e7b3ca121ec46cb10c5
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-03-23 13:07:16 +0000

    add Docker-Compose and improve startscript

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-dockerd &
-
-sleep 2
+# Check, if dockerd is running
+if ! pgrep -x "dockerd" > /dev/null; then
+  echo "dockerd not running, starting dockerd..."
+  dockerd &
+  sleep 2
+else
+  echo "dockerd is running."
+fi
 
 # By some strange reason we need to do echo command to get to the next command
 echo " "
